442ce42ef5002cf3930587e5e0cf879d4edb7167
De <valdeirtb@gmail.com>
2022-07-18 23:33:23 -0300

    primeiro commit

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+    API_URL: "http://localhost:5000",
+    MONGO_CONNECTION:
+    'mongodb+srv://<deoliveira>:<290718>@url-shortener.9e5qq.mongodb.net/?retryWrites=true&w=majority'
+    }
